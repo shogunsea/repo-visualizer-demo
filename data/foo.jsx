7b2e090e4e623e70ce4652b03a9c0e1b484e7bd4
Shogun Sea <shogunsea08@gmail.com>
2024-11-30 18:09:23 +0800

    Create foo.jsx

---
+++ data/foo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Foo = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>This is a dummy React component.</p>
+    </div>
+  );
+};
+
+export default Foo;
